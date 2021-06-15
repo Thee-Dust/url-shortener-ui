eef2e5da724e7d12c26c2dfe16e3de4d3b1d5ae5
Dustin-Har <harbaughdustin@yahoo.com>
2021-06-15 12:28:41 -0400

    Add intercept function

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,10 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+  Cypress.Commands.add('interceptUrls', () => {
+    cy.fixture('../fixture/UrlData.json')
+    .then(json => {
+      cy.intercept('http://localhost:3001/api/v1/urls', json)
+    })
+  })
