7447f4e651acf150f8254d9f38a60a55006b4512
Dustin-Har <harbaughdustin@yahoo.com>
2021-06-15 11:41:15 -0400

    Add Post request method

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,14 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postUrls = (data) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+  .then(response => response.json())
+}
